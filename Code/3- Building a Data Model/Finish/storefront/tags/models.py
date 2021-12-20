0c8a5115be79d92a262f5dc26b55f9ea654bae20
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-12-20 21:26:52 +0800

    Just do it.

---
+++ Code/3- Building a Data Model/Finish/storefront/tags/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+    # when querying data, you might wanna get an actual object that this tag might be applied to, hence the above line
